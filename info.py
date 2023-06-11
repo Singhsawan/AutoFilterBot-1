5a8ce4a0b73e1cc7ce50b323f0ac8e82b42b01e6
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-06-11 22:51:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,8 +37,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/+4nzja42ELQwzOWVl')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/bot_channel_011')
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/okboss131')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/okboss131')
 
 # Bot settings
 AUTO_FILTER = is_enabled((environ.get('AUTO_FILTER', "True")), True)
